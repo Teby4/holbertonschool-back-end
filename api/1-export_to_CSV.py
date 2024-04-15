5cd17d144e2c2c0fb0a19c6287ddb26c8f4936be
Teby4 <felipeoliveramantero@gmail.com>
2024-04-15 10:38:28 -0700

    tasks

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+import requests
+import json
+
+def get_employee(argv):
+    empleoyeid = argv[1]
+    user_response = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(empleoyeid))
+    user = user_response.json()
+    todo_response = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".format(empleoyeid))
+    todos = todo_response.json()
+
+    task_completed = 0
+    for todo in todos:
+        if todo['completed']:
+            task_completed += 1
+
+    total_tasks = len(todos)
+    print(f"Employee {user['name']} is done with tasks ({task_completed}/{total_tasks}):")
