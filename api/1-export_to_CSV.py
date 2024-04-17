85cb3ec1fd2d53ee8149505e0e73be2c0097301e
Teby4 <felipeoliveramantero@gmail.com>
2024-04-17 07:07:52 -0700

    task 2

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""task 1"""
+
+
+import requests
+import csv
+
+
+def exportdata(argv):
+    """export empleoyee data"""
+    employee_id = argv[1]
+    userlink = "https://jsonplaceholder.typicode.com/users/"
+    user_response = requests.get("{}{}".format(userlink, employee_id))
+    user = user_response.json()
+    todolink = "https://jsonplaceholder.typicode.com/todos?userId="
+    todo_response = requests.get("{}{}".format(todolink, employee_id))
+    todos = todo_response.json()
+
+    filename = f"{employee_id}.csv"
+
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+
+        for todo in todos:
+            writer.writerow([user['id'], user['name'],
+                             todo['completed'], todo['title']])
+
+
+if __name__ == "__main__":
+    import sys
+    exportdata(sys.argv)
