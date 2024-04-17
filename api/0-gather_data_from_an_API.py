5cf24588b950a97931b6ea11ebfc32d860cc1c41
Teby4 <felipeoliveramantero@gmail.com>
2024-04-17 06:03:12 -0700

    tasks

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,13 +4,14 @@
 import requests
 import json
 
+
 def get_employee(argv):
     empleoyeid = argv[1]
-    user_response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                                 .format(empleoyeid))
+    userlink = "https://jsonplaceholder.typicode.com/users/"
+    user_response = requests.get("{}{}".format(userlink, empleoyeid))
     user = user_response.json()
-    todo_response = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                                 .format(empleoyeid))
+    todolink = "https://jsonplaceholder.typicode.com/todos?userId="
+    todo_response = requests.get("{}{}".format(todolink, empleoyeid))
     todos = todo_response.json()
 
     task_completed = 0
@@ -19,11 +20,13 @@ def get_employee(argv):
             task_completed += 1
 
     total_tasks = len(todos)
-    print(f"Employee {user['name']} is done with tasks ({task_completed}/{total_tasks}):")
+    print(f"Employee {user['name']} is done with tasks"
+          f"({task_completed}/{total_tasks}):")
 
     for todo in todos:
         if todo['completed']:
             print(f"\t {todo['title']}")
+
 
 if __name__ == "__main__":
     import sys
